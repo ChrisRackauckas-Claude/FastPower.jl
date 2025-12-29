706ebd95b1f7f2c81c66472d5816ce08e2514785
ChrisRackauckas <me@chrisrackauckas.com>
2025-12-29 00:44:46 -0500

    Fix formatting in runtests.jl - add spaces around ≈

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ const GROUP = get(ENV, "GROUP", "All")
 if GROUP == "All" || GROUP == "Core"
     @testset "Fast log2" begin
         for x in 0.001:0.001:1.2 # (0, 1+something] is the domain which a controller uses
-            @test log2(x)≈fastlog2(Float32(x)) atol=1e-3
+            @test log2(x) ≈ fastlog2(Float32(x)) atol=1e-3
         end
     end
 
